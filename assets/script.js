6a44b40ee4d9e329221df1ad5de6b0554c420733
DominusSoftware <sistemadomminus@gmail.com>
2023-06-30 16:01:19 -0300

    atualização 30/06

--- assets/script.js
+++ assets/script.js
@@ -10,7 +10,6 @@ pages.forEach(page => {
         selectedPage(page);
     });
 });
-
 function selectedPage(page) {
     const pageSelected = document.querySelector(".box-item.selected");
     pageSelected.classList.remove("selected")
@@ -19,6 +18,7 @@ function selectedPage(page) {
 
     return;
 };
+// mascara no telefone
 function mascara(o,f){
     v_obj=o
     v_fun=f
@@ -41,3 +41,63 @@ window.onload = function(){
 		mascara( this, mtel );
 	}
 }
+// envio de formulario
+class FormSubmit {
+    constructor(settings) {
+      this.settings = settings;
+      this.form = document.querySelector(settings.form);
+      this.formButton = document.querySelector(settings.button);
+      if (this.form) {
+       console.log(this.form);
+        this.url = this.form.getAttribute("action");
+      }
+      this.sendForm = this.sendForm.bind(this);
+    }
+  
+    displaySuccess() {
+      this.form.innerHTML = this.settings.success;
+    }
+    displayError() {
+      this.form.innerHTML = this.settings.error;
+    }
+    getFormObject() {
+      const formObject = {};
+      const fields = this.form.querySelectorAll("[name]");
+      fields.forEach((field) => {
+        formObject[field.getAttribute("name")] = field.value;
+      });
+      return formObject;
+    }
+    onSubmission(event) {
+      event.preventDefault();
+      event.target.disabled = true;
+    }
+    async sendForm(event) {
+      try {
+        this.onSubmission(event);
+        await fetch(this.url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify(this.getFormObject()),
+        });
+        this.displaySuccess();
+      } catch (error) {
+        this.displayError();
+        throw new Error(error);
+      }
+    }
+    init() {
+      if (this.form) this.formButton.addEventListener("click", this.sendForm);
+      return this;
+    }
+  }
+  const formSubmit = new FormSubmit({
+    form: "[data-form]",
+    button: "[data-button]",
+    success: "<h1 class='success'>Mensagem enviada!</h1>",
+    error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
+  });
+  formSubmit.init();
